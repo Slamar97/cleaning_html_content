c3f671eef3da6e0332957f1f4099096ce272d699
Slamar <sabonmarcin@gmail.com>
2021-07-19 23:55:26 +0200

    added removing by tag name

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const axios = require('axios');
 const fs = require('fs');
 const STRONA_TESTOWA = `https://www.tendacn.com/product/4G07.html`;
 
-axios.get(STRONA_TESTOWA).then(({data}) => {
+axios.get(STRONA_TESTOWA).then(({
+  data
+}) => {
   const $ = cheerio.load(data);
   const tableResponsive = $('.table-responsive');
   $('html').html(tableResponsive);
@@ -12,6 +14,11 @@ axios.get(STRONA_TESTOWA).then(({data}) => {
   $('style,img').remove();
   // Remove class and id attributes
   $('div,h4,h3,h2,h1').removeAttr('class').removeAttr('id');
+
+  $('div,h4,h3,h2,h1').removeAttr('class').removeAttr('id').each(function () {
+    if (this.tagName == 'h4')
+      $(this).remove();
+  });
 
   const html = $.html().replace('<!DOCTYPE html><html lang="zh-CN">', '');
   fs.writeFile(`text2.html`, html, 'utf8', (err) => {
